80a27f7e6be88b8ac98e2688cfe31bacfa32a78e
MMcClure11 <mcclme02@luther.edu>
2020-09-08 12:02:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your functions here
+function map(sourceArray, fxn){
+  let newArray = [];
+  sourceArray.forEach(element => {
+    newArray.push(fxn(element))
+  });
+  return newArray;
+}
+
+// function reduce(array, func, startingPoint){
+//   let accumulator;
+//   let index;
+
+//   if(!!startingPoint){
+//       accumulator = startingPoint;
+//       index = 0;
+//   } else {
+//       accumulator = func(array[0],array[1]);
+//       index = 2;
+//   }
+//   for(index; index< array.length; index++){
+//       accumulator = func(accumulator, array[index])
+//   }
+//   return accumulator;
+// }
+
+function reduce(ary, reduceFn, memo){
+  for (const element of ary){
+    memo = memo === undefined ? element : reduceFn(element, memo)
+  }
+  return memo
+}
