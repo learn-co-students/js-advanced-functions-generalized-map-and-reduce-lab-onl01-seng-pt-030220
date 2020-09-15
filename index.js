39bb200be4a5c0ca9f40c2cd3af2d4c041176b59
Rkebede <rutht.kebede@gmail.com>
2020-09-15 17:17:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+const map = (array, fn) => {
+  let newArray = []
+  for(let n of array){
+    newArray.push(fn(n))
+  }
+  return newArray
+}
+
+const reduce = (array, fn, acc = 0) => {
+  for(let n of array){
+    if (acc === 0){
+      acc = n 
+    }else{
+      acc = fn(n, acc)
+    }
+  }
+  return acc
+}
