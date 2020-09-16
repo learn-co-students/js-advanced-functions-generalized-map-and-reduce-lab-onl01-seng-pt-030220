0e248e40d38d5580b228873e308738cbaef977ec
AHMED AFANDY <eng.computer1@yahoo.com>
2020-09-16 22:33:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+// Your code here
+function map(sourceArray,callBackFunc ){
+      let array=[]
+     sourceArray.forEach(el => {
+       array.push(callBackFunc(el))
+
+     } )
+     return array
+}
+
+
+function reduce(sourceArray,startingPoint =3 ){
+     sourceArray.forEach(el => {
+         return callBackFunc(el)
+   } )
+}
