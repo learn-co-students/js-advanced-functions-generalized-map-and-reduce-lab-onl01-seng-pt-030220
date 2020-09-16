20bc070697f4437196f70ff6b7eddd592d91bd43
Clannis <Dodds1990@gmail.com>
2020-09-16 17:52:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(sourceArray, func) {
+    const result = []
+    for (let i = 0; i < sourceArray.length; i++ ){
+        result.push(func(sourceArray[i]))
+    }
+    return result
+}
+
+function reduce(sourceArray, func, startingValue) {
+    let result = startingValue ? startingValue : sourceArray[0]
+    let i = startingValue ? 0 : 1
+    for (; i < sourceArray.length; i++ ){
+        result = func(sourceArray[i], result)
+    }
+    return result
+}
