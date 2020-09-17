d83c6fc9f74307f946d1435596c944142ef8b7be
AHMED AFANDY <eng.computer1@yahoo.com>
2020-09-17 05:20:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Add your functions here
-// Your code here
+
 function map(sourceArray,callBackFunc ){
       let array=[]
      sourceArray.forEach(el => {
@@ -16,21 +15,26 @@ function reduce(sourceArray,callBackFunc,startingPoint)
   let total
 
   if (startingPoint){
-      /// first when startingPoint exist do this put total= startingPoint ,because reduce start from given inintialvalue(startingPoint)
+      /// first when startingPoint exist do this put total= startingPoint
+      //,because reduce start from given inintialvalue(startingPoint),then send all element with new total values
     total = startingPoint
     sourceArray.forEach(el=>
        total =callBackFunc(total,el)
     )
-   }else
+   }
+   else
    {
-      //we put total  to first element ,because reduce by defalut will
+      //We put total  to first element ,because reduce by defalut will
       // choose first element if no startingPoint or initial value
-      //
+      //  total =sourceArray[0],when we put total =0 test not getting pased,because
+      // he said all values not true because of total =0 it's false value,and acumalator should not start with default value zero
+      //it start with first element of array
+      //total =0
       total =sourceArray[0]
       for(let i=1;i< sourceArray.length;i++){
          total =callBackFunc(total,sourceArray[i])
     }
 
   }
-return total
+  return total
 }
