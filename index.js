8c69d951a025d0fdead50d1788f9a417b99407bc
Christopher Chaison <christopherc0825@gmail.com>
2020-09-18 16:46:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(arr, callback){
+    const newArr = []
+    for(const element of arr){
+        newArr.push(callback(element))
+    }
+    return newArr
+}
+
+function reduce(arr, callback, start){
+    let total = (!!start) ? start : arr[0]
+    let i = (!!start) ? 0 : 1
+
+    for(; i < arr.length; i++){
+        total = callback(arr[i], total)
+    }
+
+    return total
+}
