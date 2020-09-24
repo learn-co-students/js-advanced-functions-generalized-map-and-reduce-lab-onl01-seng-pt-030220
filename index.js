7545c6155e2e14a2b3a7e7e08387c2aa5a5e32f1
github username <github email address>
2020-09-24 20:32:15 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+
+function map(sourceArray, fxn) {
+let newArray = [] 
+sourceArray.forEach(element => {
+    newArray.push(fxn(element))
+    return newArray
+})
+return newArray
+};
+
+
+
+function reduce(ary, reduceFn, pointer){
+    ary.forEach (element => {
+      if(pointer===undefined){
+        pointer = element
+      }else{
+        pointer = reduceFn(element, pointer)
+      }
+    })
+    return pointer
+  }
