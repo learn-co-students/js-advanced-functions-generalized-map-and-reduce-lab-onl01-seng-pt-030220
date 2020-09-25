9e9e29a5242837430698813c8c0aebed1938e926
aredmond85 <aredmond85@gmail.com>
2020-09-25 12:57:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-// Add your functions here
+function map(src, cb) {
+  let r = []
 
-function map(array, callback) { 
-    let  newArray = []
-     for (let i = 0; i < array.length; i++) { 
-       let element = array[i]
-       newArray.push(callback(element))
-     }
-     return newArray;
+  for (let i = 0; i < src.length; i++) {
+    let theElement = src[i]
+    r.push(cb(theElement))
+  }
+
+  return r;
 }
 
-const reduce = (array, callback, intial) => {
-    let memo = intial;
-    for (let i = 0; i < array.length; i++) {
-      if (i === 0 && memo === undefined) {
-        memo = array[0];
-      } else {
-        memo = callback(array[i], memo);
-      }
-    }
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
+}
+// function map(array, callback) { 
+//     let  newArray = []
+//      for (let i = 0; i < array.length; i++) { 
+//        let element = array[i]
+//        newArray.push(callback(element))
+//      }
+//      return newArray;
+// }
+
+// const reduce = (array, callback, intial) => {
+//     let memo = intial;
+//     for (let i = 0; i < array.length; i++) {
+//       if (i === 0 && memo === undefined) {
+//         memo = array[0];
+//       } else {
+//         memo = callback(array[i], memo);
+//       }
+//     }
   
-    return memo;
-  };
+//     return memo;
+//   };
