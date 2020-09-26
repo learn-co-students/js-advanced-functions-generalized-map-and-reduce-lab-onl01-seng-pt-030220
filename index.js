e0866c0f702f5d8d076608eac382a2285adcc03f
Haloren <mckennakyler@gmail.com>
2020-09-26 01:11:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(array, fn) {
+    const result = [];
+        for(let i=0; i<array.length; i++){
+            result.push(fn(array[i]));
+        }
+    return result;
+}
+
+const reduce = function(array, fn, int) {
+    let result = int;
+    for(let i=0; i<array.length; i++){
+        if (result === undefined){
+            result = array[i];
+        } else {
+            result = fn(array[i], result);
+        }
+    };
+    return result;
+}
