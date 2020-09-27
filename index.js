16ef44def006eaf5acbc84068939ff418e70aa03
antimonysayz <antimonysayz@gmail.com>
2020-09-26 20:26:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+
+function map(arr, fn){
+    let newArr = [];
+    arr.forEach(ele => {
+        newArr.push(fn(ele));
+    })
+    return newArr;
+}
+
+function reduce(arr, fn, start){
+    let initial = start;
+    for (let i = 0; i < arr.length; i++){
+        if (i === 0 && initial === undefined) {
+            initial = arr[i];
+        } else {
+            initial = fn(arr[i], initial);
+        }
+    }
+    return initial;
+}
